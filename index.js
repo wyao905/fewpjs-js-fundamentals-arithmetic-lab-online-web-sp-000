6712de09bc10f4c95651277f7d186f4cef09f997
Warren Yao <warrenyca905@gmail.com>
2020-01-23 05:46:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 10 % 6
+
+let max = Math.max(1, 7, 20)
